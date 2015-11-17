09b80ce2e945aabee248c11eb26f336e52d8065e
Tarun AGrawal <agrawaltarun01@gmail.com>
2015-11-17 23:07:42 +0530

    adding changes

---
+++ component/src/main/java/com/movieztalk/extraction/model/Actors.java
@@ -0,0 +1,14 @@
+package com.movieztalk.extraction.model;
+
+public class Actors implements Celebrity{
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
